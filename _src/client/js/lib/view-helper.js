3cedf8e44b3e24a42ae5a34ffb8777f02b3e280a
Aliaksandr Pasynkau <alxe.master@gmail.com>
2013-12-18 00:53:46 +0300

    update

--- _src/client/js/lib/view-helper.js
+++ _src/client/js/lib/view-helper.js
@@ -7,7 +7,7 @@ define(function(require, exports, module){
 
 	Handlebars.registerHelper('url', function() {
 		var options, params, routeName, _i;
-		routeName = arguments[0], params = 3 <= arguments.length ? __slice.call(arguments, 1, _i = arguments.length - 1) : (_i = 1, []), options = arguments[_i++];
+		routeName = arguments[0], params = 3 <= arguments.length ? Array.prototype.slice.call(arguments, 1, _i = arguments.length - 1) : (_i = 1, []), options = arguments[_i++];
 		return Chaplin.helpers.reverse(routeName, params);
 	});
 
