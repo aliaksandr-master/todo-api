e592676e157e8c19515968f65e13e51c8f197064
Aliaksandr Pasynkau <alxe.master@gmail.com>
2013-12-18 13:52:25 +0300

    add layout logic

--- _src/client/js/routes.js
+++ _src/client/js/routes.js
@@ -6,8 +6,8 @@ define(function(require, exports, module){
 		match('user/profile/', 'user#profile');
 		match('user/login/', 'user#login');
 		match('todo/', 'todo#index');
-		match('todo/list/:id/', 'todo#list');
-		match('todo/item/:listId/:ItemId/', 'todo#list');
+		match('todo/:listId/', 'todo#list');
+		match('todo/:listId/:ItemId/', 'todo#list');
 		match('', 'static#home');
 		match('about/', 'static#about');
 	};
