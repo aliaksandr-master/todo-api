0fb5bda1e0efa907f32b7da67b426474bf42c907
Alexandr Karpovich <alex.karpovi4@gmail.com>
2014-01-05 19:05:32 +0300

    todo-module items + database

--- server/application/controllers/todo.php
+++ server/application/controllers/todo.php
@@ -7,18 +7,19 @@ class Todo extends REST_Controller {
     public function __construct() {
         parent::__construct();
         $this->loader()->model('Todo_model');
+        $this->loader()->model('TodoItem_model');
         $this->loader()->library('data_transfer/Data_transfer');
     }
 
-    public function list_get($arg = null){
+    public function list_get($id = null){
         $dataTransfer = new Data_Transfer();
         $todoObject = new Todo_model();
         $where = null;
-        if (!empty($arg)) {
-            $where = array('id' => $arg);
+        if (!is_null($id)) {
+            $where = array('id' => $id);
         }
         $todoArray = $todoObject->read($where);
-        $dataTransfer->data('todo_lists', $todoArray);
+        $dataTransfer->data('list', $todoArray);
         $this->response($dataTransfer->getAllData());
     }
 
@@ -29,8 +30,8 @@ class Todo extends REST_Controller {
         } else {
             $data = array();
             $data['name'] = trim($this->post('name'));
-            $data['date_creation'] = date("Y-m-d h:i:s", gettimeofday(true));
-            $data['sort_order'] = $this->post('sort_order');
+            $data['date_create'] = date("Y-m-d H:i:s", gettimeofday(true));
+            $data['link'] = md5(gettimeofday(true).rand(1,1100)).gettimeofday(true);
 
             $todoObject = new Todo_model();
             $todoId = $todoObject->create($data);
@@ -39,22 +40,34 @@ class Todo extends REST_Controller {
         $this->response($dataTransfer->getAllData());
     }
 
-    public function item_get($arg = null){
+    public function item_get($todoId = null, $id = null){
+        $dataTransfer = new Data_Transfer();
+        $todoItemObject = new TodoItem_model();
+        $where['todo_id'] = $todoId;
+        if (!is_null($id)) {
+            $where['id'] = $id;
+        }
+        $todoItemArray = $todoItemObject->read($where);
+        $dataTransfer->data('item', $todoItemArray);
+        $this->response($dataTransfer->getAllData());
+    }
 
+    public function item_post(){
         $dataTransfer = new Data_Transfer();
         if(empty($this->_post_args['name'])){
             $dataTransfer->error()->field('name', 'required');
         } else {
             $data = array();
+            $data['todo_id'] = trim($this->post('todo_id'));
             $data['name'] = trim($this->post('name'));
-            $data['date_creation'] = date("Y-m-d h:i:s", gettimeofday(true));
-            $data['sort_order'] = $this->post('sort_order');
+            $data['date_create'] = date("Y-m-d H:i:s", gettimeofday(true));
 
-            $todoObject = new Todo_model();
-            $todoId = $todoObject->create($data);
-            $dataTransfer->data('todo_id', $todoId);
+            $todoItemObject = new TodoItem_model();
+            $todoItemId = $todoItemObject->create($data);
+            $dataTransfer->data('item_id', $todoItemId);
         }
         $this->response($dataTransfer->getAllData());
     }
+
 
 }
