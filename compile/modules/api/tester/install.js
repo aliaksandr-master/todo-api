6fd0001de0f29edcfaa372816bde444d8479489e
Aliaksandr Pasynkau <alxe.master@gmail.com>
2014-05-05 17:25:04 +0300

    fix ignored tmp files

--- compile/modules/api/tester/install.js
+++ compile/modules/api/tester/install.js
@@ -3,24 +3,25 @@
 module.exports = function (grunt) {
 	var path = this.path;
 
-	this.config('clean', [
-		path.BUILD + '/api-test'
-	]);
+	this
+		.config('clean', [
+			path.BUILD + '/api-test'
+		])
 
-	this.config('copy', {
-		options: {
-			excludeEmpty: true
-		},
-		files: [
-			{
-				expand: true,
-				cwd: path.SRC + '/opt/api-test',
-				src: [
-					'**/*.{php,html,htaccess,hbs,js,css,eot,svg,ttf,woff,otf}',
-					'**/.htaccess'
-				],
-				dest: path.BUILD + '/api-test'
-			}
-		]
-	});
+		.config('copy', {
+			options: {
+				excludeEmpty: true
+			},
+			files: [
+				{
+					expand: true,
+					cwd: path.SRC + '/opt/api-test',
+					src: [
+						'**/*.{php,html,htaccess,hbs,js,css,eot,svg,ttf,woff,otf}',
+						'**/.htaccess'
+					],
+					dest: path.BUILD + '/api-test'
+				}
+			]
+		});
 };
